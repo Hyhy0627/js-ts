c4b98b582e217a2caed9db56a421de5aefa57872
Nguyen Quang Huy <quanghuygc2006@gmail.com>
2025-11-09 20:28:57 +0700

    chapter 3

---
+++ src/Chapter 3/video17/video17.js
@@ -0,0 +1,5 @@
+console.log("video 17");
+
+var fullName = 'Quang Huy';
+
+console.log(fullName);
---
+++ src/Chapter 3/video18/video18.js
@@ -0,0 +1,22 @@
+// number 
+const myAge = 19;
+const myCore = 9.5;
+
+console.log('video 18');
+console.log(myAge);
+console.log(myCore);
+
+// string
+const name = 'Quang Huy';
+const name1 = "Huy Quang";
+const name2 = `Huy Quang Nguyen`;
+
+console.log(name);
+
+// boolean
+const isSuccess = true;
+console.log(isSuccess);
+
+// null & undefined
+let bla;
+console.log(bla);
