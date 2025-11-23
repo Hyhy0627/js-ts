faf7b1d4e66714d3d39beac49c8b2e401158370a
Nguyen Quang Huy <quanghuygc2006@gmail.com>
2025-11-23 22:54:26 +0700

    chapter 4

--- src/chapter 4/video32/video32.js
+++ src/chapter 4/video32/video32.js
@@ -1,8 +1,18 @@
-console.log('video 32');
+ console.log('video 32');
+
+// const sum = (a, b, c) => {
+//     console.log('run before return');
+//     if (typeof a !== 'number') {
+//         console.log('run empty return');
+//         return; // void
+//     }
+//     return a + b + c;
+//     console.log('run before return');
+// } // Arrow function with three parameters
+
+// console.log(sum(1, 2, 3)); // Outputs: 6
 
 const sum = (a, b, c) => {
-    return a + b + c;
-    console.log('run after return');
-} // Arrow function with three parameters
-
+    return a + b + c; // void
+}
 console.log(sum(1, 2, 3)); // Outputs: 6
